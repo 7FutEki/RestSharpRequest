4db82fe40c185100c84d5e5b93fd21c87f0ecb64
7FutEki <senchaaa3@gmauil.com>
2023-09-17 23:01:47 +0400

    Добавьте файлы проекта.

---
+++ TestApi/Metods.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApi
+{
+    public class Metods
+    {
+
+        public List<Good> GetGoods(string host, string geturl)
+        {
+            var options = new RestClientOptions(host);
+            var client = new RestClient(options);
+            var request = new RestRequest(geturl);
+            var response = client.Execute(request);
+            List<Good> s = JsonConvert.DeserializeObject<List<Good>>(response.Content);
+            return s;
+        }
+
+
+        public void PostGood(string host, string posturl, string token, Good good)
+        {
+            var options = new RestClientOptions(host);
+            var client = new RestClient(options);
+            var request = new RestRequest(posturl);
+            var json = JsonConvert.SerializeObject(good);
+            request.AddJsonBody(json);
+            request.AddHeader("token", token);
+            client.ExecutePost(request);
+        }
+
+
+        public void DeleteGood(string host, string deleteurl, string token, Guid guidGood)
+        {
+            var options = new RestClientOptions(host);
+            var client = new RestClient(options);
+            var request = new RestRequest(deleteurl);
+            request.AddHeader("token", token);
+            request.AddHeader("guidGood", guidGood);
+            client.Delete(request);
+        }
+
+    }
+}
---
+++ TestApi/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using TestApi;
+
+string host = "http://localhost:5000/Catalog/";
+string token = "a7b25d781e794003b3e4e1e43fa34ded";
+string urlget =  $"GetGoods?token={token}";
+string urladd = "AddGood";
+string urldelete = "DeleteGood";
+
+Good good = new Good()
+{
+    idGood = Guid.NewGuid(),
+    name = "testdelete",
+    price = 0
+};
+
+Metods m = new Metods();
+m.PostGood(host, urladd, token, good);
+m.DeleteGood(host, urldelete, token, good.idGood);
+List<Good> s = m.GetGoods(host, urlget);
+
+foreach (var item in s)
+{
+    Console.WriteLine(item.idGood);
+    Console.WriteLine(item.name);
+    Console.WriteLine(item.price);
+}
+
